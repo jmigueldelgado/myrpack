f78c96800d2838dff1488c5d6b4d49de32e104d3
jmigueldelgado <jmigueldelgado@gmail.com>
2017-09-29 00:36:48 +0200

    creates scripts for converting long data frames into WASA input files

--- R/lib_WASA.R
+++ R/lib_WASA.R
@@ -561,6 +561,34 @@ read_rainfall_hourly <- function(l)
         return(xtsObj)
     }
 
+#' writes WASA input file P
+#' @param long is a long data frame object whose points are the centroids of the subbasins
+#' @param address is the path where the file should be written
+#' @export
+long2WASA_P <- function(long,address)
+{
+    try(system(paste0("mkdir ",address)))
+    try(system(paste0("rm ",address,"/rain_daily.dat")))
+    fileConn <- file(paste0(address,"/rain_daily.dat"),"a")
+    cat("Daily total precipitation [mm] for each subasin, ordered according to Map-IDs","Date\t\tSubbasin-ID.", file = fileConn, sep = "\n")
+
+    dfObj1 <- select(long,date) %>%
+        distinct(date) %>%
+        mutate(str=strftime(date,"%d%m%Y"),index=row_number()) %>%
+        select(-date)
+    
+    dfObj2 <- select(long,-x,-y) %>%
+        spread(location,value) %>%
+        select(-date)
+    dfObj <- bind_cols(dfObj1,dfObj2)
+    
+    HEADER <- c("0","0",colnames(dfObj2))
+    cat(HEADER, file = fileConn, sep = "\t")
+    cat("\n",file = fileConn, sep = "")
+    write.table(dfObj,file = fileConn, quote = FALSE, sep = "\t", row.names = FALSE, col.names=FALSE, fileEncoding = "UTF-8")
+    close(fileConn)            
+}
+
 
 #' writes WASA input file T
 #' @param stObj is a spacetime object whose points are the centroids of the subbasins
@@ -569,7 +597,7 @@ read_rainfall_hourly <- function(l)
 spacetime2WASA_T <- function(stObj,address)
 {
     df <- as(stObj,"data.frame")
-    mtx <- t(matrix(df$values,ncol=length(stObj@time),nrow=length(stObj@sp@data$ids)))
+    mtx <- t(matrix(df$value,ncol=length(stObj@time),nrow=length(stObj@sp)))
 
     try(system(paste0("mkdir ",address)))
     try(system(paste0("rm ",address,"/temperature.dat")))
@@ -591,6 +619,64 @@ spacetime2WASA_T <- function(stObj,address)
     write.table(dfObj,file = fileConn, quote = FALSE, sep = "\t", row.names = FALSE, col.names=FALSE, fileEncoding = "UTF-8")
     close(fileConn)            
 }
+
+#' writes WASA input file T
+#' @param stObj is a spacetime object whose points are the centroids of the subbasins
+#' @param address is the path where the file should be written
+#' @export
+long2WASA_T <- function(long,address)
+{
+    
+    try(system(paste0("mkdir ",address)))
+    try(system(paste0("rm ",address,"/temperature.dat")))
+    fileConn <- file(paste0(address,"/temperature.dat"),"a")
+    cat("Daily average temperature (in degree Celsius)","Date\tNo. of days\tSubbasin-ID.", file = fileConn, sep = "\n")
+
+    dfObj1 <- select(long,date) %>%
+        distinct(date) %>%
+        mutate(str=strftime(date,"%d%m%Y"),index=row_number()) %>%
+        select(-date)
+    
+    dfObj2 <- select(long,-x,-y) %>%
+        spread(location,value) %>%
+        select(-date)
+    dfObj <- bind_cols(dfObj1,dfObj2)
+    
+    HEADER <- c("0","0",colnames(dfObj2))
+    cat(HEADER, file = fileConn, sep = "\t")
+    cat("\n",file = fileConn, sep = "")
+    write.table(dfObj,file = fileConn, quote = FALSE, sep = "\t", row.names = FALSE, col.names=FALSE, fileEncoding = "UTF-8")
+    close(fileConn)            
+}
+
+#' writes WASA input file R
+#' @param long is a long data frame object whose points are the centroids of the subbasins
+#' @param address is the path where the file should be written
+#' @export
+long2WASA_R <- function(long,address)
+{
+    try(system(paste0("mkdir ",address)))
+    try(system(paste0("rm ",address,"/radiation.dat")))
+    fileConn <- file("./radiation.dat","a")
+    cat("Daily average radiation [W/m2]","Date\tNo. of days\tSubbasin-ID.", file = fileConn, sep = "\n")
+
+    dfObj1 <- select(long,date) %>%
+        distinct(date) %>%
+        mutate(str=strftime(date,"%d%m%Y"),index=row_number()) %>%
+        select(-date)
+    
+    dfObj2 <- select(long,-x,-y) %>%
+        spread(location,value) %>%
+        select(-date)
+    dfObj <- bind_cols(dfObj1,dfObj2)
+    
+    HEADER <- c("0","0",colnames(dfObj2))
+    cat(HEADER, file = fileConn, sep = "\t")
+    cat("\n",file = fileConn, sep = "")
+    write.table(dfObj,file = fileConn, quote = FALSE, sep = "\t", row.names = FALSE, col.names=FALSE, fileEncoding = "UTF-8")
+    close(fileConn)            
+}
+
 
 #' writes WASA input file R
 #' @param stObj is a spacetime object whose points are the centroids of the subbasins
@@ -614,6 +700,34 @@ spacetime2WASA_R <- function(stObj)
     colnames(dfObj2) <- as.numeric(stObj@sp@data$ids)
     dfObj <- data.frame(dfObj1,format(dfObj2,digits=0,nsmall=1, scientific=FALSE))
     HEADER <- c(colnames(dfObj1),colnames(dfObj2))
+    cat(HEADER, file = fileConn, sep = "\t")
+    cat("\n",file = fileConn, sep = "")
+    write.table(dfObj,file = fileConn, quote = FALSE, sep = "\t", row.names = FALSE, col.names=FALSE, fileEncoding = "UTF-8")
+    close(fileConn)            
+}
+
+#' writes WASA input file H
+#' @param long is a long data frame object whose points are the centroids of the subbasins
+#' @param address is the path where the file should be written
+#' @export
+long2WASA_H <- function(long,address)
+{
+    try(system(paste0("mkdir ",address)))
+    try(system(paste0("rm ",address,"/humidity.dat")))
+    fileConn <- file(paste0(address,"/humidity.dat"),"a")
+    cat("Daily average humidity [%]","Date\tNo. of days\tSubbasin-ID.", file = fileConn, sep = "\n")
+
+    dfObj1 <- select(long,date) %>%
+        distinct(date) %>%
+        mutate(str=strftime(date,"%d%m%Y"),index=row_number()) %>%
+        select(-date)
+    
+    dfObj2 <- select(long,-x,-y) %>%
+        spread(location,value) %>%
+        select(-date)
+    dfObj <- bind_cols(dfObj1,dfObj2)
+    
+    HEADER <- c("0","0",colnames(dfObj2))
     cat(HEADER, file = fileConn, sep = "\t")
     cat("\n",file = fileConn, sep = "")
     write.table(dfObj,file = fileConn, quote = FALSE, sep = "\t", row.names = FALSE, col.names=FALSE, fileEncoding = "UTF-8")
